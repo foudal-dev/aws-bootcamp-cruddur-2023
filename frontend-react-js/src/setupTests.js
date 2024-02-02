022eb3cbcecc26f19ffb457d6fb917ee4d009b74
foudal-dev <62546312+foudal-dev@users.noreply.github.com>
2024-02-02 11:59:39 +0100

    Initial commit

---
+++ frontend-react-js/src/setupTests.js
@@ -0,0 +1,5 @@
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/***REMOVED***ing-library/jest-dom
+import '@***REMOVED***ing-library/jest-dom';
